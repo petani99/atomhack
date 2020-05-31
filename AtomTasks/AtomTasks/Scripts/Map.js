7de7254f2b4330c0d3e2d4a86f2bfec5a7885be2
Andrey Dubovik <dai66@mail.ru>
2020-05-31 13:02:30 +0500

    123321

--- AtomTasks/AtomTasks/Scripts/Map.js
+++ AtomTasks/AtomTasks/Scripts/Map.js
@@ -11,10 +11,10 @@ function init() {
         objectManager = new ymaps.ObjectManager();
 
     myMap.geoObjects.add(objectManager);
-    alert(x + '\n' + y + '\n' + z)
+   
     $.ajax({
-        // В файле data.json заданы геометрия, опции и данные меток .
-        url: 'http://localhost:52724/home/taskpoints/'
+        url: '/api/tasks/'
+        , beforeSend: function () {alert("Test")}
         , success: function (data) { objectManager.add(data); }
         , error: function (x, y, z) { alert(x + '\n' + y + '\n' + z); }});
 
